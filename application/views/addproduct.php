8247e6c6a48f372242917bdb3b8db6cdc8d6f523
Rafael Montero <98540602+mckadv@users.noreply.github.com>
2022-01-27 17:58:59 -0500

    Update addproduct.php

--- application/views/addproduct.php
+++ application/views/addproduct.php
@@ -13,37 +13,37 @@
 			?>
 			<div class="content"> 
 				<form action="" method="POST" enctype="multipart/form-data" name="coffe">
-          <div class="form-group">
-            <label for="name">Nombre del producto</label>
-            <input type="text" id="name" name="name" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="reference">Referencia</label>
-            <textarea id="reference" name="reference" rows="4" class="form-control" required></textarea>
-          </div>
-          <div class="form-group">
-            <label for="price">Precio</label>
-            <input type="number" id="price" name="price" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="weight">Peso</label>
-            <input type="number" id="weight" name="weight" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="categories">Categoría</label>
-            <input type="text" id="categories" name="categories" class="form-control" required>
-          </div>
-          <div class="form-group">
-            <label for="stock">Existencia:</label>
-            <input type="number" id="stock" name="stock" class="form-control" required>
-          </div>
+				  <div class="form-group">
+				    <label for="name">Nombre del producto</label>
+				    <input type="text" id="name" name="name" class="form-control" required>
+				  </div>
+				  <div class="form-group">
+				    <label for="reference">Referencia</label>
+				    <textarea id="reference" name="reference" rows="4" class="form-control" required></textarea>
+				  </div>
+				  <div class="form-group">
+				    <label for="price">Precio</label>
+				    <input type="number" id="price" name="price" class="form-control" required>
+				  </div>
+				  <div class="form-group">
+				    <label for="weight">Peso</label>
+				    <input type="number" id="weight" name="weight" class="form-control" required>
+				  </div>
+				  <div class="form-group">
+				    <label for="categories">Categoría</label>
+				    <input type="text" id="categories" name="categories" class="form-control" required>
+				  </div>
+				  <div class="form-group">
+				    <label for="stock">Existencia:</label>
+				    <input type="number" id="stock" name="stock" class="form-control" required>
+				  </div>
           
-					<?php if ($this->uri->segment(2) === 'update') { ?>
-						<button type="submit" name="submit" class="btn btn-success" value="edit"><?= ' Editar ' ?></button>
-					<?php } else { ?>
-						<button type="submit" id="submit" name="submit" class="btn btn-success" value="save"><?= ' Registrar ' ?></button>
-					<?php } ?>        
-				</form>
+				<?php if ($this->uri->segment(2) === 'update') { ?>
+					<button type="submit" name="submit" class="btn btn-success" value="edit"><?= ' Editar ' ?></button>
+				<?php } else { ?>
+					<button type="submit" id="submit" name="submit" class="btn btn-success" value="save"><?= ' Registrar ' ?></button>
+				<?php } ?>        
+			</form>
 		</div> <!-- /.col-sm-12 -->
 	</div> <!-- /.row -->
 </div> <!-- /.container -->          
