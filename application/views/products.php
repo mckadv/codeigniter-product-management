83034ac7e114921871e0544b3758f52c59bd32d4
Rafael Montero <98540602+mckadv@users.noreply.github.com>
2022-01-27 17:15:29 -0500

    Create products.php

---
+++ application/views/products.php
@@ -0,0 +1,49 @@
+<div class="container"> 	
+	<div class="row"> 
+		<div class="col-sm-12 col-md-12 col-lg-12">
+			<?php
+			$messages = $this->session->flashdata('result_publish');
+			if ( $this->session->flashdata('result_delete') || $this->session->flashdata('result_publish') ) {
+				?> 
+          <div class="alert alert-success alert-dismissible text-center" role="alert">
+              <span type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></span>
+              <?= $this->session->flashdata('result_publish')??$this->session->flashdata('result_delete') ?>
+          </div>
+				<?php
+			}
+			?>
+ 		</div> <!-- /.col-sm-12 -->
+  </div> <!-- /.row -->     
+      
+  <div class="row"> 
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Referencia</th>
+                <th>Precio</th>
+                <th>Peso</th>
+                <th>Categoría</th>
+                <th>Existencia</th>
+                <th>Actualizar</th>
+                <th>Eliminar</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($products as $row) { ?>
+            <tr>
+                <td><?php echo $row->name ?></td>
+                <td><?php echo $row->reference ?></td>
+                <td><?php echo $row->price ?></td>
+                <td><?php echo $row->weight ?></td>
+                <td><?php echo $row->categories ?></td>
+                <td><?php echo $row->stock ?></td>
+                <td><a class="btn btn-warning" href="<?php echo base_url() ."products/update/" . $row->id ?>"><i class="fa fa-edit"></i></a></td>
+                <td><a class="btn btn-danger" href="<?php echo base_url() ."products/delete/" . $row->id ?>"><i class="fa fa-trash"></i></a></td>
+            </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+  </div> <!-- /.row -->
+</div> <!-- /.container -->
+      
