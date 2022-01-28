a0ee0cbe7c49e5914b70df363798115f38964e2e
Rafael Montero <98540602+mckadv@users.noreply.github.com>
2022-01-28 08:23:04 -0500

    Update products.php

--- application/views/products.php
+++ application/views/products.php
@@ -3,11 +3,11 @@
 		<div class="col-sm-12 col-md-12 col-lg-12">
 			<?php
 			$messages = $this->session->flashdata('result_publish');
-			if ( $this->session->flashdata('result_delete') || $this->session->flashdata('result_publish') ) {
+			if ( $this->session->flashdata('result_delete') || $this->session->flashdata('result_publish') || $this->session->flashdata('result_cart') ) {
 				?> 
           <div class="alert alert-success alert-dismissible text-center" role="alert">
               <span type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></span>
-              <?= $this->session->flashdata('result_publish')??$this->session->flashdata('result_delete') ?>
+              <?= $this->session->flashdata('result_publish')??isset($this->session->flashdata('result_delete'))?$this->session->flashdata('result_delete'):$this->session->flashdata('result_cart') ?>
           </div>
 				<?php
 			}
@@ -27,6 +27,7 @@
                 <th>Existencia</th>
                 <th>Actualizar</th>
                 <th>Eliminar</th>
+		<th>Vender</th>
             </tr>
         </thead>
         <tbody>
@@ -40,6 +41,7 @@
                 <td><?php echo $row->stock ?></td>
                 <td><a class="btn btn-primary" href="<?php echo base_url() ."products/addproduct/" . $row->id ?>"><i class="fa fa-edit"></i></a></td>
                 <td><a class="btn btn-warning" href="<?php echo base_url() ."products/addproduct/" . $row->id ?>"><i class="fa fa-trash"></i></a></td>
+		<td><a class="btn btn-success" href="<?php echo base_url() ."products/sellproduct/" . $row->id ?>"><i class="fa fa-opencart"></i></a></td>
             </tr>
             <?php } ?>
         </tbody>
